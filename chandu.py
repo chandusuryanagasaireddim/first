33630eb345e43bedb92ae7a433ae4e6a1fc7992c
chandusuryanagasaireddim <sireeshchandu888@gmail.com>
2021-11-19 23:46:43 +0530

    fan conversation

--- chandu.py
+++ chandu.py
@@ -1,1 +1,5 @@
-print("I am fan of Abdul kalam sir")
+fan=input("you are fan of ? ")
+if fan=="abdul kalam":
+   print("I am fan of Abdul kalam sir too")
+else :
+    print("Oh, I am fan of Abdul Kalam sir")
